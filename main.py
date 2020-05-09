ee0b85ef951ae48f388705b1e11cb07c2bba27e7
Paulo B F Almeida <51975269+pauloBFalmeida@users.noreply.github.com>
2020-05-08 23:01:51 -0300

    change movement to follow mouse
    now u cant escape the screen :)

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ import random
 g.init()
 gd=g.display
 gt=g.time
+gm=g.mouse
 m=255
 t=9
+r=1
+q=0
 w=gd.set_mode((m,m))
 p=[150,m-t,(0,m,0)]
 ce=(m,0,0)
 en=[]
 gt.set_timer(25,99)
-r=1
-q=0
 while r:
 	gt.Clock().tick(60)
 	for e in g.event.get():
 		if e.type==g.QUIT:r=0
 		if e.type==25:en+=[[random.randint(0,m),0,ce]]
-	ks=g.key.get_pressed()
-	if ks[g.K_a]:p[0]-=3
-	if ks[g.K_d]:p[0]+=3
+	p[0]=gm.get_pos()[0]
 	for e in list(en):
 		e[1]+=7
 		if e[1]>m-t:
@@ -30,4 +29,5 @@ while r:
 	for z in en+[p]:g.draw.rect(w,z[2],(z[0],z[1],t,t))
 	gd.update()
 g.quit()
+print('made by Paulo BF Almeida')
 print(q)
